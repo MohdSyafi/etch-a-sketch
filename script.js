b29c4530391d4f0a9f4f36269f5ee3798e6a9ecd
syafi <syafimohamad13@gmail.com>
2022-09-02 10:15:32 +0800

    completed the project

--- script.js
+++ script.js
@@ -1,8 +1,54 @@
 
+let btn = document.querySelector('.button');
 const gridContainer = document.querySelector(".grid-container");
+let squareNo = 16;
 
-for (let i =0;i<32;i++){
-    const gridDiv = document.createElement("div");
-    gridDiv.textContent = "1";
-    gridContainer.appendChild(gridDiv);
+function createGrid(){
+    for (let i =0;i<squareNo;i++){
+        const gridRow = document.createElement("div");
+        gridContainer.appendChild(gridRow);
+    
+        for(let j = 0;j<squareNo;j++){
+            const gridBox = document.createElement("div");
+            gridRow.appendChild(gridBox);
+            gridRow.classList.add('grid-Row');
+            gridBox.classList.add('grid-box');
+    
+            gridBox.addEventListener('mouseover',changeBoxColor)
+        }
+    
+    }
 }
+
+function clearBox()
+{
+    const container = document.querySelector(".grid-container");
+    container.innerHTML = "";
+}
+
+function changeBoxColor(){
+    this.classList.add('changeBoxColor');
+}
+
+function showPrompt(){
+
+   squareNo = parseInt(prompt('No of squares per side :', "16"));
+
+   if (!Number.isInteger(squareNo)){
+    alert("Only number is accepted");
+    squareNo = 16;
+    return;
+   }
+
+   if(squareNo>100){
+    alert("Maximum no of square is 100");
+    squareNo = 16;
+    return;
+   }
+
+   clearBox();
+   createGrid();
+}
+
+btn.addEventListener('click',showPrompt);
+
